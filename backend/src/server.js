112993fa9b4503e87fca759ba607de920196ad60
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-08-23 23:59:25 +0500

    feat: integrate Clerk for user authentication and management, integrate Stream, Sentry Inngest & make connection between MongoDB and Clerk by Inngest function

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { ENV } from "./config/env.js"
 import { connectDB } from "./config/db.js"
-
+import { clerkMiddleware } from "@clerk/express"
 
 const app = express()
 
+app.use(clerkMiddleware())
 app.get("/", (req,res) => {
     res.send("Hello world!")
 })
